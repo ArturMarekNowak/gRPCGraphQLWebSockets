2b0578294ecc6c5d97a06b248e6024526e8cdeee
Artur Nowak <arturmareknowal@gmail.com>
2022-03-20 17:25:30 +0100

    implemented gRPC working toy example

--- gRPCGraphQLWebSockets/Services/MessagesService.cs
+++ gRPCGraphQLWebSockets/Services/MessagesService.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 using gRPCGraphQLWebSockets.Database;
 using gRPCGraphQLWebSockets.Model;
 using gRPCGraphQLWebSockets.Services.Intefaces;
